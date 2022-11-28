13521693b62bec0e543622b830372ceac4a548c9
tomneo2004 <tomneo2004@gmail.com>
2022-11-28 10:38:27 +0800

    Smoothing player turning direction

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,17 +2,30 @@ class_name Player
 extends KinematicBody2D
 
 # Player movement speed
-export var movement_speed := 200.0
+export var movement_speed := 250.0
+
+# How fast can player turn from 
+# one direction to another
+#
+# The higher the value the faster player can turn
+# and less the smooth of player motion
+export (float, 0.1, 1.0) var drag_factor := 0.5
 
 # Player current velocity
 var velocity := Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
     # Get direction from input
     var direction: Vector2 = Vector2(
         Input.get_axis("move_left", "move_right"),
         Input.get_axis("move_up", "move_down")
     ).normalized()
 
-    velocity = direction * movement_speed
+    # Smoothing player turing direction
+    var desired_velocity := direction * movement_speed
+    var steering_velocity = desired_velocity - velocity
+    steering_velocity  = steering_velocity * drag_factor
+    velocity += steering_velocity
+
+    # Move player
     velocity = move_and_slide(velocity)
