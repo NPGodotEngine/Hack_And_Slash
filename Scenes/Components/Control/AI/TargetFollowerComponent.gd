71b3d8b0067fcc8400f88a735250efa974847abb
tomneo2004 <tomneo2004@gmail.com>
2023-03-21 15:36:28 +0800

    feature: target follower now can permanent follow target once target is detected

--- Scenes/Components/Control/AI/TargetFollowerComponent.gd
+++ Scenes/Components/Control/AI/TargetFollowerComponent.gd
@@ -11,13 +11,20 @@ export(NodePath) var actor: NodePath
 
 export (bool) var enable_follow: bool = false
 export (float) var keep_distance: float = 100.0
-export(float) var keep_dist_threshold: float = 1.0
+export (float) var keep_dist_threshold: float = 1.0
+
+# Once target is assigned, follow the target
+# permanently until new target is assigned
+##
+# Target will never out of range
+export (bool) var permanent_target: bool = false
 
 onready var _movement: MovementComponent = get_node(movement) as MovementComponent
 onready var _target_detector: TargetDetector = get_node(target_detector) as TargetDetector
 onready var _actor: KinematicBody2D = get_node(actor) as KinematicBody2D
 
-var _target = null
+# The target this follower will move to
+var target = null
 
 
 func _get_configuration_warning() -> String:
@@ -40,7 +47,9 @@ func _ready() -> void:
 		return
 
 	_target_detector.connect("target_detected", self, "_on_target_detected")
-	_target_detector.connect("target_lost", self, "_on_target_lost")
+	
+	if not permanent_target:
+		_target_detector.connect("target_lost", self, "_on_target_lost")
 
 func _physics_process(delta: float) -> void:
 	if Engine.editor_hint:
@@ -50,21 +59,21 @@ func _physics_process(delta: float) -> void:
 		move_close_to_target()
 
 func _on_target_detected(detected_context:TargetDetector.DetectedContext) -> void:
-	if _target == null:
-		_target = detected_context.detected_target
+	if target == null:
+		target = detected_context.detected_target
 
 func _on_target_lost(target_lost_context:TargetDetector.TargetLostContext) -> void:
-	if _target:
-		_target = null
+	if target:
+		target = null
 
 
 
 func move_close_to_target():
-	if _target == null or _movement == null or _actor == null: 
+	if target == null or _movement == null or _actor == null: 
 		return
 
-	var direction: Vector2 = _target.global_position - _actor.global_position
-	var dist_to_target: float = _actor.global_position.distance_to(_target.global_position)
+	var direction: Vector2 = target.global_position - _actor.global_position
+	var dist_to_target: float = _actor.global_position.distance_to(target.global_position)
 
 	if (is_equal_approx(keep_dist_threshold, abs(dist_to_target - keep_distance)) or 
 		abs(dist_to_target - keep_distance) < keep_dist_threshold):
