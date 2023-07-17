8a2bc55edebf6a6746732925e431e17579e05dbd
tomneo2004 <tomneo2004@gmail.com>
2023-07-17 17:58:24 +0800

    pathfinding in test

---
+++ Levels/Navigator.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+export (NodePath) var agent: NodePath
+onready var nav_agent: NavigationAgent2D = get_node(agent) as NavigationAgent2D
+
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	nav_agent.path_desired_distance = 4.0
+	nav_agent.target_desired_distance = 4.0
+
+	nav_agent.connect("velocity_computed", self, "_on_velocity_computed")
+
+	call_deferred("actor_setup")
+
+func actor_setup() -> void:
+	yield(get_tree(), "physics_frame")
+	set_movement_target(get_parent().global_position)
+
+func set_movement_target(movement_target: Vector2):
+	nav_agent.set_target_location(movement_target)
+
+func _physics_process(_delta: float) -> void:
+	if Input.is_action_pressed("primary"):
+		set_movement_target(get_global_mouse_position())
+		return
+
+	if nav_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = get_parent().global_position
+	var next_path_position: Vector2 = nav_agent.get_next_location()
+	print(next_path_position)
+
+	var new_velocity: Vector2 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	if nav_agent.avoidance_enabled:
+		nav_agent.set_velocity(new_velocity)
+	else:
+		_on_velocity_computed(new_velocity)
+
+func _on_velocity_computed(safe_velocity: Vector2) -> void:
+	(get_parent().get_node("MovementComponent") as MovementComponent).process_move(safe_velocity)
