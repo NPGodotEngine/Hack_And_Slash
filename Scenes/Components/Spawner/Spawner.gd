4b1594a7c77a4cf6029d31ddc990318241de6abf
tomneo2004 <tomneo2004@gmail.com>
2023-07-26 17:23:12 +0800

    add enemy spawner

--- Scenes/Components/Spawner/Spawner.gd
+++ Scenes/Components/Spawner/Spawner.gd
@@ -7,6 +7,7 @@ extends Sprite2D
 @export var list :Array[String] = []
 @export_range(0, 100) var spawn_chance_percent :int = 100
 @export var num_spawn :int = 1
+@export var spawn_radius: float = 50.0
 
 func _ready() -> void:
 	if Engine.is_editor_hint():
@@ -29,3 +30,29 @@ func spawn() -> void:
 
 func handle_spawn(_resource_name:String) -> void:
 	pass
+
+## Get a random point as `Vector2` from a circle
+func random_point_inside_circle(center:Vector2, min_radius:float, max_radius:float):
+	var random_vector = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+	random_vector = random_vector.normalized()
+	random_vector *= randf_range(min_radius, max_radius)
+	return center + random_vector
+
+func draw_circle_arc(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PackedVector2Array()
+
+	for i in range(nb_points + 1):
+		var angle_point = deg_to_rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+
+	for index_point in range(nb_points):
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color, 2.0)
+
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint():
+		queue_redraw()
+
+func _draw() -> void:
+	if Engine.is_editor_hint():
+		draw_circle_arc(Vector2.ZERO, spawn_radius, 0, 360, Color(1.0, 0.0, 0.0))
